f68caf3878e29713ebff7b8edb3a8ad3457b0e08
John Wall <5672618@qq.com>
2019-11-08 21:22:08 +0800

    init

---
+++ src/Yhsb.Test/CsharpTest.csx
@@ -0,0 +1,7 @@
+#! "netcoreapp2.1"
+
+#r "../../bin/Debug/netstandard2.0/Yhsb.dll"
+
+using Yhsb;
+
+Util.Say("Hello");
---
+++ src/Yhsb/Util/Util.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Yhsb 
+{
+    public static class Util
+    {
+        public static void Say(string word) => Console.WriteLine(word);
+    }
+}
