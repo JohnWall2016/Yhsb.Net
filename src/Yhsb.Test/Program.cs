11f7bf22bbc7d879fb967e67b4e4866c45b82052
John Wall <5672618@qq.com>
2019-11-09 11:05:49 +0800

    update

--- src/Yhsb.Test/Program.cs
+++ src/Yhsb.Test/Program.cs
@@ -10,25 +10,26 @@ namespace Yhsb.Test
         {
             if (args.Length <= 0)
             {
-                Console.WriteLine("dotnet run --project src/Yhsb.Test/ [-l] <method>");
+                Console.WriteLine("dotnet run --project src/Yhsb.Test/ [-l|-a] <method>");
                 return;
             }
             var list = false;
+            var matchAll = false;
             string pattern = null;
 
             foreach (var arg in args)
             {
                 var a = arg.Trim();
-                if (a == "-l")
-                {
-                    list = true;
-                }
+                if (a == "-l") list = true;
+                else if (a == "-a") matchAll = true;
                 else if (a != "")
                 {
                     pattern = a;
                     break;
                 }
             }
+
+            Console.WriteLine($"Arguments: [List: {list}, MatchAll: {matchAll}, Pattern: {pattern}]");
 
             var type = typeof(TestRunner);
             foreach (var t in type.Assembly.GetTypes())
@@ -41,17 +42,28 @@ namespace Yhsb.Test
                         if (list) Console.WriteLine("  " + m.Name);
                         if (pattern != null)
                         {
-                            var match = Regex.Match(m.Name, pattern, RegexOptions.IgnoreCase);
-                            if (match.Success)
+                            try
                             {
-                                if (!list)
+                                var match = Regex.Match(t.Name + "." + m.Name, pattern, RegexOptions.IgnoreCase);
+                                if (match.Success)
                                 {
-                                    Console.WriteLine(t.Name + ":");
-                                    Console.WriteLine("  " + m.Name);
+                                    if (!list)
+                                    {
+                                        Console.WriteLine(t.Name + ":");
+                                        Console.WriteLine("  " + m.Name);
+                                    }
+                                    Console.WriteLine("".PadLeft(27, '-') + "OUTPUT" + "".PadLeft(27, '-'));
+                                    m.Invoke(null, null);
+                                    Console.WriteLine("".PadLeft(60, '-'));
                                 }
+                                if (!matchAll) return;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("".PadLeft(27, '-') + "ERROR" + "".PadLeft(28, '-'));
+                                Console.WriteLine($"{ex}");
                                 Console.WriteLine("".PadLeft(60, '-'));
-                                m.Invoke(null, null);
-                                Console.WriteLine("".PadLeft(60, '-'));
+                                return;
                             }
                         }
                     }
