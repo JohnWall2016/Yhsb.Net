122d730fc983fdbec0c0ea7357bc28ad7edaf991
JohnWall2016 <5672618@qq.com>
2019-11-15 11:56:42 +0800

    update

---
+++ src/Yhsb.Jb.Query/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CommandLine;
+using Yhsb.Util.Command;
+
+namespace Yhsb.Jb.Query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Command.Parse<Jfxx, Doc>(args);
+        }
+    }
+
+    [Verb("jfxx", HelpText = "缴费信息查询")]
+    class Jfxx : ICommand
+    {
+        [Option('e', "export", HelpText = "导出信息表")]
+        public bool Export { get; set; }
+
+        [Value(0, HelpText = "身份证号码",
+            Required = true)]
+        public string IDCard { get; set; }
+
+        public void Execute()
+        {
+            Session.Use(session =>
+            {
+                session.SendService(new JfxxQuery(IDCard));
+                var result = session.GetResult<Jb.Jfxx>();
+
+                foreach (var info in result.Data)
+                {
+                    Console.WriteLine(info.ToJson());
+                }
+            });
+        }
+    }
+
+    [Verb("doc", HelpText = "档案目录生成")]
+    class Doc : ICommand
+    {
+        [Value(0, HelpText = "xlsx文件",
+            Required = true)]
+        public string Xlsx { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(Xlsx);
+        }
+    }
+}
---
+++ src/Yhsb/Util/Command/Command.cs
@@ -0,0 +1,65 @@
+using CommandLine;
+using CommandLine.Text;
+using System.Collections.Generic;
+
+namespace Yhsb.Util.Command
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class Command
+    {
+        static Command()
+        {
+            SentenceBuilder.Factory =
+                () => new LocalizableSentenceBuilder();
+        }
+
+        public static ParserResult<T> Parse<T>(IEnumerable<string> args)
+            where T : ICommand
+        {
+            return Parser.Default.ParseArguments<T>(args)
+                .WithParsed(exec => exec.Execute());
+        }
+
+        public static ParserResult<object> Parse<T1, T2>(IEnumerable<string> args)
+            where T1 : ICommand where T2 : ICommand
+        {
+            return Parser.Default.ParseArguments<T1, T2>(args)
+                .WithParsed(exec => (exec as ICommand).Execute());
+        }
+
+        public static ParserResult<object> Parse<T1, T2, T3>(IEnumerable<string> args)
+            where T1 : ICommand where T2 : ICommand where T3 : ICommand
+        {
+            return Parser.Default.ParseArguments<T1, T2, T3>(args)
+                .WithParsed(exec => (exec as ICommand).Execute());
+        }
+
+        public static ParserResult<object> Parse<T1, T2, T3, T4>(IEnumerable<string> args)
+            where T1 : ICommand where T2 : ICommand where T3 : ICommand
+            where T4 : ICommand
+        {
+            return Parser.Default.ParseArguments<T1, T2>(args)
+                .WithParsed(exec => (exec as ICommand).Execute());
+        }
+
+        public static ParserResult<object> Parse<T1, T2, T3, T4, T5>(IEnumerable<string> args)
+            where T1 : ICommand where T2 : ICommand where T3 : ICommand
+            where T4 : ICommand where T5 : ICommand
+        {
+            return Parser.Default.ParseArguments<T1, T2, T3, T4, T5>(args)
+                .WithParsed(exec => (exec as ICommand).Execute());
+        }
+
+        public static ParserResult<object> Parse<T1, T2, T3, T4, T5, T6>(IEnumerable<string> args)
+            where T1 : ICommand where T2 : ICommand where T3 : ICommand
+            where T4 : ICommand where T5 : ICommand where T6 : ICommand
+        {
+            return Parser.Default.ParseArguments<T1, T2, T3, T4, T5, T6>(args)
+                .WithParsed(exec => (exec as ICommand).Execute());
+        }
+    }
+}
