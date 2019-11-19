abde3842c40a2674d2f39c8af376c2f001e39fdd
JohnWall2016 <5672618@qq.com>
2019-11-19 16:44:52 +0800

    update

---
+++ src/Yhsb.Test/DbTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Yhsb.Jb;
+using System.Linq;
+
+public class DbTest
+{
+    public static void TestFPTable()
+    {
+        using var context = new JzfpContext();
+        var data = from fpData in context.FPTable2019
+                   where fpData.IDCard == "430311194610131520"
+                   select fpData;
+        if (data.Any())
+        {
+            var info = data.First();
+            Console.WriteLine($"{info.IDCard} {info.Name} {info.JBClass}");
+        }
+    }
+}
---
+++ src/Yhsb/Jb/Database.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Yhsb.Jb
+{
+    [Table("2019年度扶贫历史数据底册")]
+    public class FPTable2019
+    {
+        [Column("姓名")]
+        public string Name { get; set; }
+
+        [Key, Column("身份证号码")]
+        public string IDCard { get; set; }
+
+        [Column("居保认定身份")]
+        public string JBClass { get; set; }
+    }
+
+    public class JzfpContext : DbContext
+    {
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql(_internal.Database.DBConnectString);
+
+        public DbSet<FPTable2019> FPTable2019 { get; set; }
+    }
+}
