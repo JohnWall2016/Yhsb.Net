9780efedeebd1a4016bad800820aeb99cf5a2a82
JohnWall2016 <5672618@qq.com>
2019-11-22 17:00:37 +0800

    update

--- src/Yhsb.Test/CsharpTest.cs
+++ src/Yhsb.Test/CsharpTest.cs
@@ -1,3 +1,4 @@
+using System;
 using static System.Console;
 
 class CsharpTest
@@ -16,8 +17,30 @@ class CsharpTest
                 "  社保经办机构 划拨时间");
 
         WriteLine(
-            $"{"序号", 2}{"年度", 3}{"个人缴费", 6}{"省级补贴", 5}" +
-            $"{"市级补贴", 5}{"县级补贴", 5}{"政府代缴", 5}{"集体补助", 5}" +
+            $"{"序号",2}{"年度",3}{"个人缴费",6}{"省级补贴",5}" +
+            $"{"市级补贴",5}{"县级补贴",5}{"政府代缴",5}{"集体补助",5}" +
             "  社保经办机构 划拨时间");
+    }
+
+    public static void TestSwitch()
+    {
+        static void f(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                    WriteLine("One");
+                    goto default;
+                case "2":
+                    WriteLine("Two");
+                    goto case "1";
+
+                default:
+                    WriteLine("Def");
+                    break;
+            }
+        }
+        f("1");
+        f("2");
     }
 }
