ea8ce402565ed2c64adce729d94b0da7c696d918
JohnWall2016 <5672618@qq.com>
2020-05-12 11:42:05 +0800

    update

--- src/Yhsb.Jb.FullCover/Program.cs
+++ src/Yhsb.Jb.FullCover/Program.cs
@@ -1,12 +1,87 @@
-﻿using System;
+﻿using CommandLine;
+using Yhsb.Util.Command;
+using Yhsb.Util.Excel;
+using System.IO;
+using System.Collections.Generic;
+
+using static System.Console;
 
 namespace Yhsb.Jb.FullCover
 {
     class Program
     {
+        [App(Name = "全覆盖处理程序")]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Command.Parse<Split>(args);
+        }
+    }
+
+    [Verb("split", HelpText = "数据分发程序")]
+    class Split : ICommand
+    {
+        [Value(0, HelpText = "源数据表路径",
+            Required = true, MetaName = "SourceExcel")]
+        public string SourceExcel { get; set; }
+
+        [Value(1, HelpText = "开始行，从1开始",
+            Required = true)]
+        public int BeginRow { get; set; }
+
+        [Value(2, HelpText = "结束行(包含)，从1开始",
+            Required = true)]
+        public int EndRow { get; set; }
+        
+        [Value(3, HelpText = "分组模板表路径",
+            Required = true, MetaName = "TemplateExcel")]
+        public string TemplateExcel { get; set; }
+
+        [Value(4, HelpText = "输出目录",
+            Required = true, MetaName = "OutDir")]
+        public string OutDir { get; set; }
+
+        public void Execute()
+        {
+            var workbook = ExcelExtension.LoadExcel(SourceExcel);
+            var sheet = workbook.GetSheetAt(0);
+
+            WriteLine("生成分组映射表");
+            var map = new Dictionary<string, List<int>>();
+            for (var index = BeginRow - 1; index < EndRow; index++)
+            {
+                var xzj = sheet.Row(index).Cell("F").Value();
+                if (!map.ContainsKey(xzj))
+                    map[xzj] = new List<int>();
+                map[xzj].Add(index);
+            }
+
+            WriteLine("生成分组数据表");
+            foreach (var xzj in map.Keys)
+            {
+                var count = map[xzj].Count;
+                WriteLine($"{xzj}: {count}");
+
+                if (count <= 0) continue;
+
+                var outWorkbook = ExcelExtension.LoadExcel(TemplateExcel);
+                var outSheet = outWorkbook.GetSheetAt(0);
+
+                int startRow = 1, currentRow = startRow;
+                (int Begin, int End) cols = (1, 7);
+
+                map[xzj].ForEach(rowIndex => {
+                    var index = currentRow - startRow + 1;
+                    var inRow = sheet.Row(rowIndex);
+                    var outRow = outSheet.GetOrCopyRow(currentRow++, startRow);
+                    outRow.Cell("A").SetValue(index);
+                    for (var col = cols.Begin; col <= cols.End; col++) {
+                        outRow.Cell(col).SetValue(inRow.Cell(col).Value());
+                    }
+                });
+                outWorkbook.Save(
+                    Path.Join(OutDir, $"{xzj}{map[xzj].Count}.xls"));
+                //break;
+            }
         }
     }
 }
